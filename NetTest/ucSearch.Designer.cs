6a5a829ea2acf0c79b99f84f4b2ae21bfb0ed96c
Tony Stoddart <github@explose.co.uk>
2019-03-20 13:03:08 +0000

    Add new files

---
+++ NetTest/ucSearch.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace NetTest
+{
+    partial class ucSearch
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.lvSearch = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(81, 32);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(346, 20);
+            this.tbSearch.TabIndex = 0;
+            // 
+            // lvSearch
+            // 
+            this.lvSearch.Location = new System.Drawing.Point(81, 80);
+            this.lvSearch.Name = "lvSearch";
+            this.lvSearch.Size = new System.Drawing.Size(346, 97);
+            this.lvSearch.TabIndex = 1;
+            this.lvSearch.UseCompatibleStateImageBehavior = false;
+            // 
+            // ucSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lvSearch);
+            this.Controls.Add(this.tbSearch);
+            this.Name = "ucSearch";
+            this.Size = new System.Drawing.Size(579, 247);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.ListView lvSearch;
+    }
+}

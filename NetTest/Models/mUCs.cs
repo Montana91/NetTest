3b91b6ee9454166e96a70e8a1b14b1a446b3d97d
Tony Stoddart <supertonework@hotmail.com>
2019-10-03 05:47:58 +0100

    Added class to handle all the user controls

---
+++ NetTest/Models/mUCs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTest.UserControls;
+namespace NetTest.Models
+{
+    public class mUCs
+    {
+
+
+        ucTransactions m_ucTransactions;
+        ucLogin m_ucLogin;
+        ucLoggedIn m_ucLoggedIn;
+    }
+}
